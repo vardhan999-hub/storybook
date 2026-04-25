a3c1dee4e2a792b6ca4eb3cc8d03d0975c55977b
Harsha14 <harshavtadigadapa14@gmail.com>
2026-04-25 12:00:03 +0530

    main.js

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [
+            ["@babel/preset-react", { runtime: "automatic" }],
+            "@babel/preset-env",
+          ],
+        },
+      },
+    });
+    config.resolve.extensions.push(".js", ".jsx");
+    return config;
+  },
+};
+
+export default config;
